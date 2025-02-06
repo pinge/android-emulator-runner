95e5ce5aaea5312a6d53ed14f47f53912bb04d6b
Nuno Pinge <nuno@boxtobox.app>
2025-02-06 04:25:05 +0100

    debug

--- src/emulator-manager.ts
+++ src/emulator-manager.ts
@@ -76,9 +76,11 @@ export async function launchEmulator(
       },
     });
 
+    console.log('Getting locale..')
     // wait for emulator to complete booting
     const localeMatch = emulatorOptions.match(/ -change-locale ([a-z]{2}-[A-Z]{2})/)
     const locale = localeMatch === null ? undefined : localeMatch[1]
+    console.log(`Locale: ${locale}`)
     await waitForDevice(port, emulatorBootTimeout, locale);
     await adb(port, `shell input keyevent 82`);
 
